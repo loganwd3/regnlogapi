ed14ff9092faf29fe315736b94cfa0de60a4513c
logangit2 <loganathanavon@gmail.com>
2024-02-26 16:52:58 +0530

    updated connection url

--- index.js
+++ index.js
@@ -12,13 +12,28 @@ const userModel = require("./Models/usermodel");
 //middleware
 app.use(express.json());
 app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accespt"
+  );
+  next();
+});
 mongoose
-  .connect("mongodb://localhost:27017/registerform")
+  .connect(
+    "mongodb+srv://logandb2:logan@cluster0.2wc2eqb.mongodb.net/registerform",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => {
     console.log("db connected");
   })
   .catch((err) => {
-    console.log(err);
+    console.log("Some problem -" + err);
   });
 
 //user register
